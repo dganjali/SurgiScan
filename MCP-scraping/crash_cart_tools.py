66dd1203847d3409b6d2e16ed098459fcf6e3e1f
Arjan Waraich <135071914+RJN25@users.noreply.github.com>
2025-08-02 05:13:37 -0400

    Create crash_cart_tools.py

---
+++ MCP-scraping/crash_cart_tools.py
@@ -0,0 +1,313 @@
+"""
+Comprehensive Crash Cart Tools Database
+Organized by drawer categories with dosages and quantities
+"""
+
+CRASH_CART_TOOLS = {
+    "drawer_1_airway_breathing": [
+        "Bag-Valve-Mask (BVM) - Adult size",
+        "Bag-Valve-Mask (BVM) - Pediatric size",
+        "Bag-Valve-Mask (BVM) - Infant size",
+        "Oropharyngeal airway (OPA) - Size 40mm",
+        "Oropharyngeal airway (OPA) - Size 50mm",
+        "Oropharyngeal airway (OPA) - Size 60mm",
+        "Oropharyngeal airway (OPA) - Size 70mm",
+        "Oropharyngeal airway (OPA) - Size 80mm",
+        "Oropharyngeal airway (OPA) - Size 90mm",
+        "Oropharyngeal airway (OPA) - Size 100mm",
+        "Nasopharyngeal airway (NPA) - Size 6.0mm",
+        "Nasopharyngeal airway (NPA) - Size 7.0mm",
+        "Nasopharyngeal airway (NPA) - Size 8.0mm",
+        "Endotracheal tube (ETT) - Size 6.0mm",
+        "Endotracheal tube (ETT) - Size 6.5mm",
+        "Endotracheal tube (ETT) - Size 7.0mm",
+        "Endotracheal tube (ETT) - Size 7.5mm",
+        "Endotracheal tube (ETT) - Size 8.0mm",
+        "Endotracheal tube (ETT) - Size 8.5mm",
+        "Laryngoscope handle",
+        "Laryngoscope blade - Macintosh #3",
+        "Laryngoscope blade - Macintosh #4",
+        "Laryngoscope blade - Miller #2",
+        "Laryngoscope blade - Miller #3",
+        "Lubricant jelly - 5g packet",
+        "End-tidal CO2 detector",
+        "Colorimetric capnometer",
+        "Laryngeal Mask Airway (LMA) - Size 3",
+        "Laryngeal Mask Airway (LMA) - Size 4",
+        "Laryngeal Mask Airway (LMA) - Size 5",
+        "Stylet - 6.0mm",
+        "Stylet - 7.0mm",
+        "Stylet - 8.0mm",
+        "Bougie introducer - 15F",
+        "Suction catheter - 14F",
+        "Suction catheter - 16F",
+        "Yankauer suction tip",
+        "Tracheostomy kit",
+        "Nasal cannula - Adult",
+        "Nasal cannula - Pediatric",
+        "Non-rebreather mask - Adult",
+        "Non-rebreather mask - Pediatric"
+    ],
+    
+    "drawer_2_circulation_iv_access": [
+        "IV catheter - 14G",
+        "IV catheter - 16G",
+        "IV catheter - 18G",
+        "IV catheter - 20G",
+        "IV catheter - 22G",
+        "Intraosseous (IO) access kit - EZ-IO",
+        "IV start kit - Tourniquet",
+        "IV start kit - Antiseptic swabs",
+        "IV start kit - Tape",
+        "IV start kit - Gauze pads",
+        "Pressure infusion bag",
+        "Blood draw tubes - Red top",
+        "Blood draw tubes - Purple top",
+        "Blood draw tubes - Blue top",
+        "Normal saline (0.9% NaCl) - 1000ml bag",
+        "Normal saline (0.9% NaCl) - 500ml bag",
+        "Lactated Ringer's - 1000ml bag",
+        "Lactated Ringer's - 500ml bag",
+        "Dextrose 5% in water (D5W) - 1000ml bag",
+        "Dextrose 5% in water (D5W) - 500ml bag"
+    ],
+    
+    "drawer_3_medications_acls_emergency": [
+        # Cardiac Arrest Medications
+        "Epinephrine (1:10,000) - 1mg/10ml vial",
+        "Epinephrine (1:10,000) - 1mg/10ml pre-filled syringe",
+        "Amiodarone - 150mg/3ml vial",
+        "Amiodarone - 300mg/6ml vial",
+        "Lidocaine - 100mg/5ml vial",
+        "Lidocaine - 200mg/10ml vial",
+        "Atropine - 1mg/10ml vial",
+        "Atropine - 1mg/10ml pre-filled syringe",
+        
+        # Anaphylaxis Medications
+        "Epinephrine (1:1,000) - 1mg/1ml auto-injector",
+        "Epinephrine (1:1,000) - 1mg/1ml vial",
+        "Diphenhydramine - 50mg/1ml vial",
+        "Methylprednisolone - 125mg/2ml vial",
+        
+        # Sedatives/Paralytics (RSI)
+        "Etomidate - 20mg/10ml vial",
+        "Ketamine - 100mg/1ml vial",
+        "Ketamine - 500mg/5ml vial",
+        "Succinylcholine - 200mg/10ml vial",
+        "Rocuronium - 50mg/5ml vial",
+        "Rocuronium - 100mg/10ml vial",
+        
+        # Seizure Medications
+        "Midazolam - 5mg/1ml vial",
+        "Midazolam - 10mg/2ml vial",
+        "Lorazepam - 2mg/1ml vial",
+        "Lorazepam - 4mg/1ml vial",
+        "Diazepam - 10mg/2ml vial",
+        
+        # Other Emergency Medications
+        "Sodium bicarbonate - 50mEq/50ml vial",
+        "Calcium chloride - 1g/10ml vial",
+        "Calcium gluconate - 1g/10ml vial",
+        "Dextrose 50% - 25g/50ml vial",
+        "Magnesium sulfate - 2g/10ml vial",
+        "Naloxone (Narcan) - 0.4mg/1ml vial",
+        "Naloxone (Narcan) - 2mg/2ml vial",
+        "Adenosine - 6mg/2ml vial",
+        "Adenosine - 12mg/4ml vial",
+        "Nitroglycerin spray - 0.4mg/spray",
+        "Nitroglycerin tablets - 0.4mg sublingual"
+    ],
+    
+    "drawer_4_defibrillation_cardiac_monitoring": [
+        "Defibrillator pads - Adult",
+        "Defibrillator pads - Pediatric",
+        "Defibrillator - Manual/AED combination",
+        "ECG electrodes - 3-lead",
+        "ECG electrodes - 5-lead",
+        "ECG electrodes - 12-lead",
+        "ECG leads - 3-lead cable",
+        "ECG leads - 5-lead cable",
+        "ECG leads - 12-lead cable",
+        "Pediatric defibrillator paddles",
+        "Cardiac monitor cables",
+        "Conductive gel - 120g tube"
+    ],
+    
+    "drawer_5_procedure_miscellaneous": [
+        "Central line kit - Triple lumen",
+        "Central line kit - Quadruple lumen",
+        "Chest tube insertion tray",
+        "Needle decompression kit - 14G needles",
+        "Suture kit - 2-0 silk",
+        "Suture kit - 3-0 silk",
+        "Suture kit - 4-0 silk",
+        "Scalpel - #10 blade",
+        "Scalpel - #11 blade",
+        "Scalpel - #15 blade",
+        "Forceps - Hemostat",
+        "Forceps - Tissue",
+        "Scissors - Mayo",
+        "Scissors - Metzenbaum",
+        "Alcohol wipes - 70% isopropyl",
+        "Betadine swabs",
+        "Chlorhexidine swabs",
+        "Sterile gloves - Size 7",
+        "Sterile gloves - Size 7.5",
+        "Sterile gloves - Size 8",
+        "Sterile gloves - Size 8.5",
+        "Sterile gowns - Medium",
+        "Sterile gowns - Large",
+        "Sterile masks",
+        "Tourniquet - Combat application",
+        "Tourniquet - Emergency medical",
+        "Tape - Silk 1 inch",
+        "Tape - Paper 1 inch",
+        "Tape - Transparent 1 inch",
+        "Sharps container - 1L capacity"
+    ]
+}
+
+# Create a flat list of all tools
+ALL_CRASH_CART_TOOLS = []
+for category, tools in CRASH_CART_TOOLS.items():
+    ALL_CRASH_CART_TOOLS.extend(tools)
+
+# Comprehensive tool synonyms for matching
+TOOL_SYNONYMS = {
+    # Airway & Breathing
+    "epinephrine": ["adrenaline", "epi", "1:1000", "1:10000", "1mg/ml", "1mg/10ml"],
+    "atropine": ["atropine sulfate", "1mg/10ml"],
+    "amiodarone": ["cordarone", "150mg/3ml", "300mg/6ml"],
+    "lidocaine": ["xylocaine", "100mg/5ml", "200mg/10ml"],
+    "defibrillator": ["aed", "automated external defibrillator", "manual defibrillator"],
+    "laryngoscope": ["laryngoscope handle", "laryngoscope blade", "macintosh", "miller"],
+    "endotracheal": ["ett", "endotracheal tube", "6.0mm", "7.0mm", "8.0mm"],
+    "ambu": ["bag valve mask", "bvm", "bag-valve-mask"],
+    "syringe": ["syringes", "pre-filled syringe"],
+    "needle": ["needles", "14g", "16g", "18g", "20g", "22g"],
+    "catheter": ["iv catheter", "peripheral iv", "central line"],
+    "gloves": ["sterile gloves", "size 7", "size 8"],
+    "gauze": ["gauze pads", "sterile gauze"],
+    "tape": ["silk tape", "paper tape", "transparent tape"],
+    "tourniquet": ["combat application tourniquet", "emergency tourniquet"],
+    "stethoscope": ["stethoscope"],
+    "scalpel": ["scalpel blade", "#10 blade", "#11 blade", "#15 blade"],
+    "suture": ["suture kit", "2-0 silk", "3-0 silk", "4-0 silk"],
+    "oxygen": ["nasal cannula", "non-rebreather mask", "face mask"],
+    "mask": ["face mask", "oxygen mask", "non-rebreather"],
+    "tubing": ["iv tubing", "oxygen tubing"],
+    "medication": ["drug", "med", "vial", "ampule"],
+    "monitor": ["cardiac monitor", "ecg monitor", "ekg monitor"],
+    "ecg": ["ekg", "electrocardiogram", "electrocardiography"],
+    "ekg": ["ecg", "electrocardiogram", "electrocardiography"],
+    "pulse oximeter": ["spo2", "oxygen saturation"],
+    "blood pressure": ["bp", "sphygmomanometer", "cuff"],
+    "thermometer": ["temp", "temperature"],
+    "glucometer": ["glucose meter", "blood glucose"],
+    "capnography": ["end-tidal co2", "etco2", "capnometer"],
+    "blood gas": ["arterial blood gas", "abg"],
+    "bp cuff": ["blood pressure cuff", "sphygmomanometer"],
+    
+    # Medications
+    "dopamine": ["dopamine hcl", "inotropic"],
+    "vasopressin": ["antidiuretic hormone", "adh"],
+    "calcium": ["calcium chloride", "calcium gluconate", "1g/10ml"],
+    "sodium": ["sodium bicarbonate", "50meq/50ml"],
+    "magnesium": ["magnesium sulfate", "2g/10ml"],
+    "adenosine": ["adenocard", "6mg/2ml", "12mg/4ml"],
+    "nitroglycerin": ["nitro", "0.4mg", "sublingual"],
+    "furosemide": ["lasix", "diuretic"],
+    "morphine": ["morphine sulfate", "opioid"],
+    "ketamine": ["ketamine hcl", "100mg/1ml", "500mg/5ml"],
+    "succinylcholine": ["sux", "200mg/10ml"],
+    "rocuronium": ["zemuron", "50mg/5ml", "100mg/10ml"],
+    "midazolam": ["versed", "5mg/1ml", "10mg/2ml"],
+    "propofol": ["diprivan", "sedative"],
+    "dexamethasone": ["decadron", "steroid"],
+    "diphenhydramine": ["benadryl", "50mg/1ml"],
+    "methylprednisolone": ["solu-medrol", "125mg/2ml"],
+    "etomidate": ["amidate", "20mg/10ml"],
+    "lorazepam": ["ativan", "2mg/1ml", "4mg/1ml"],
+    "diazepam": ["valium", "10mg/2ml"],
+    "naloxone": ["narcan", "0.4mg/1ml", "2mg/2ml"],
+    "dextrose": ["d50", "d5w", "25g/50ml"],
+    
+    # Airway
+    "laryngeal mask airway": ["lma", "supraglottic airway"],
+    "nasopharyngeal": ["npa", "nasal airway"],
+    "oropharyngeal": ["opa", "oral airway"],
+    "cricothyrotomy": ["emergency tracheostomy", "surgical airway"],
+    "bougie": ["gum elastic bougie", "introducer"],
+    "co2": ["carbon dioxide", "end-tidal co2", "etco2"],
+    "cannula": ["nasal cannula", "oxygen cannula"],
+    
+    # Vascular
+    "iv": ["intravenous", "peripheral iv", "central line"],
+    "intraosseous": ["io", "ez-io", "bone access"],
+    "tegaderm": ["transparent dressing", "iv dressing"],
+    "central line": ["central venous catheter", "cvp"],
+    "heparin": ["heparin sodium", "anticoagulant"],
+    
+    # Surgical
+    "chest tube": ["thoracostomy", "chest drain"],
+    "thoracotomy": ["chest surgery", "emergency thoracotomy"],
+    "foley": ["foley catheter", "urinary catheter"],
+    "ng tube": ["nasogastric tube", "feeding tube"],
+    "bandages": ["elastic bandage", "ace wrap"],
+    
+    # Monitoring
+    "defibrillator": ["aed", "automated external defibrillator", "manual defibrillator"],
+    "pads": ["defibrillator pads", "adult pads", "pediatric pads"],
+    "paddles": ["defibrillator paddles", "manual paddles"],
+    "electrodes": ["ecg electrodes", "ekg electrodes", "3-lead", "5-lead", "12-lead"],
+    "leads": ["ecg leads", "ekg leads", "monitor cables"],
+    "conductive gel": ["defibrillator gel", "ecg gel"]
+}
+
+def get_all_tools():
+    """Get all crash cart tools as a flat list"""
+    return ALL_CRASH_CART_TOOLS
+
+def get_tools_by_category():
+    """Get tools organized by category"""
+    return CRASH_CART_TOOLS
+
+def match_tool(input_tool):
+    """Match input tool against crash cart tools using synonyms"""
+    if not input_tool:
+        return None
+    
+    input_lower = input_tool.lower()
+    
+    # Direct match
+    for tool in ALL_CRASH_CART_TOOLS:
+        if input_lower in tool.lower() or tool.lower() in input_lower:
+            return tool
+    
+    # Synonym match
+    for synonym, alternatives in TOOL_SYNONYMS.items():
+        if synonym in input_lower:
+            for tool in ALL_CRASH_CART_TOOLS:
+                if any(alt in tool.lower() for alt in alternatives):
+                    return tool
+    
+    # Reverse synonym match
+    for tool in ALL_CRASH_CART_TOOLS:
+        tool_lower = tool.lower()
+        for synonym, alternatives in TOOL_SYNONYMS.items():
+            if synonym in tool_lower and any(alt in input_lower for alt in alternatives):
+                return tool
+    
+    return None
+
+def get_tools_by_drawer():
+    """Get tools organized by drawer"""
+    drawer_names = {
+        "drawer_1_airway_breathing": "🟥 Drawer 1: Airway & Breathing",
+        "drawer_2_circulation_iv_access": "🟧 Drawer 2: Circulation & IV Access", 
+        "drawer_3_medications_acls_emergency": "🟨 Drawer 3: Medications (ACLS/Emergency)",
+        "drawer_4_defibrillation_cardiac_monitoring": "🟩 Drawer 4: Defibrillation & Cardiac Monitoring",
+        "drawer_5_procedure_miscellaneous": "🟦 Drawer 5: Procedure & Miscellaneous Tools"
+    }
+    
+    return {drawer_names.get(k, k): v for k, v in CRASH_CART_TOOLS.items()} 
